0d0d58d805ed9af65f7740cd1f8c464becd0bda1
dt <dthompson.wa@gmail.com>
2020-08-04 09:11:07 -0700

    Fixed snapshot coordinate error. Added code to hide open tooltips.

--- Babel/Viewfinder.cs
+++ Babel/Viewfinder.cs
@@ -5,6 +5,8 @@ using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using System.Threading;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Babel
 {
@@ -73,13 +75,14 @@ namespace Babel
         }
 
         // Takes a screenshot of what's behind the window and returns it
-        // BUG: There are several coordinate issues that result in snap getting a few pixels off
         private Image Snap()
         {
-            int x = PointToScreen(tscMain.ContentPanel.Location).X;
-            int y = PointToScreen(tscMain.ContentPanel.Location).Y;
+            int x = pbxDisplay.RectangleToScreen(pbxDisplay.ClientRectangle).X;
+            int y = pbxDisplay.RectangleToScreen(pbxDisplay.ClientRectangle).Y;
             int width = pbxDisplay.Size.Width;
             int height = pbxDisplay.Size.Height;
+
+            WindowFunctions.HideAllTooltips(); // Hide all open tooltips since the user might be hovering over a toolbar button
 
             IntPtr hdcSrc = GDI32.User32.GetWindowDC(GDI32.User32.GetDesktopWindow());
             IntPtr hdcDst = GDI32.CreateCompatibleDC(hdcSrc);
@@ -87,6 +90,8 @@ namespace Babel
             GDI32.SelectObject(hdcDst, bitmap);
 
             GDI32.BitBlt(hdcDst, 0, 0, width, height, hdcSrc, x, y, GDI32.TernaryRasterOperations.SRCCOPY);
+
+            
 
             return Image.FromHbitmap(bitmap);
         }
@@ -666,5 +671,97 @@ namespace Babel
         }
     }
 
+    class WindowFunctions
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
 
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        public const int SW_HIDE = 0;
+
+        // Delegate to filter which windows to include 
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        /// <summary> Get the text for the window pointed to by hWnd </summary>
+        public static string GetWindowText(IntPtr hWnd)
+        {
+            int size = GetWindowTextLength(hWnd);
+            if (size > 0)
+            {
+                var builder = new StringBuilder(size + 1);
+                GetWindowText(hWnd, builder, builder.Capacity);
+                return builder.ToString();
+            }
+
+            return String.Empty;
+        }
+
+        /// <summary> Find all windows that match the given filter </summary>
+        /// <param name="filter"> A delegate that returns true for windows
+        ///    that should be returned and false for windows that should
+        ///    not be returned </param>
+        public static IEnumerable<IntPtr> FindWindows(EnumWindowsProc filter)
+        {
+            IntPtr found = IntPtr.Zero;
+            List<IntPtr> windows = new List<IntPtr>();
+
+            EnumWindows(delegate (IntPtr wnd, IntPtr param)
+            {
+                if (filter(wnd, param))
+                {
+                    // only add the windows that pass the filter
+                    windows.Add(wnd);
+                }
+
+                // but return true here so that we iterate all windows
+                return true;
+            }, IntPtr.Zero);
+
+            return windows;
+        }
+
+        /// <summary> Find all windows that contain the given title text </summary>
+        /// <param name="titleText"> The text that the window title must contain. </param>
+        public static IEnumerable<IntPtr> FindWindowsWithText(string titleText)
+        {
+            return FindWindows(delegate (IntPtr wnd, IntPtr param)
+            {
+                return GetWindowText(wnd).Contains(titleText);
+            });
+        }
+
+        public static bool DummyEnumWindowsProc(IntPtr hWnd, IntPtr param)
+        {
+            return true;
+        }
+
+        // This method will find and hide all open tooltips systemwide
+        public static void HideAllTooltips()
+        {
+            var allToolTips = FindWindows(DummyEnumWindowsProc);
+            foreach (IntPtr handle in allToolTips)
+            {
+                StringBuilder classbuilder = new StringBuilder(100);
+                GetClassName(handle, classbuilder, 100);
+                if (classbuilder.ToString().Contains("tooltips"))
+                {
+                    if (IsWindowVisible(handle))
+                    {
+                        ShowWindow(handle, SW_HIDE); // Hide tooltip
+                    }
+                }
+            }
+        }
+    }
 }
