e041e8dc63a6828b41bc8618109d3b5c1a1fa368
dt <dthompson.wa@gmail.com>
2020-08-09 09:21:19 -0700

    Improved documentation

--- Babel/BabelMethods.cs
+++ Babel/BabelMethods.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using Babel.Windows;
-using Babel.Google;
 using System.Windows.Input;
 using System.Configuration;
 
